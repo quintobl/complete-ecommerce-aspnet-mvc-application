6f37cc25487e613a976df4fa2df83f8f6a0142a2
Brad Quinton <bradley.l.quinton@gmail.com>
2022-09-17 01:37:39 -0400

    Adding latest batch of files.

---
+++ eTickets/Data/AppDbContextcs.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(
+                m => m.MovieId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
